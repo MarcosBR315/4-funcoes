9a2736bd805469f6073045c1480d45f94e0d7548
MarcosBR315 <marcospaulo.pontes05@gmail.com>
2022-06-16 13:47:35 -0300

    4 - Atividade de Funções VnW

---
+++ script.js
@@ -0,0 +1,26 @@
+let msgUm;
+let meuNome;
+let nome;
+let idade;
+let estiloMusical;
+
+function mensagemNoConsole(){
+    console.log("Olá, Marilene!");
+}
+mensagemNoConsole();
+
+console.log("\n");
+
+function exibirMeuNome(meuNome){
+    console.log(meuNome);
+}
+exibirMeuNome ("Marcos Paulo Oliveira Pontes (AKA [Also Known As] MarcosBR315)");
+console.log("Mandei até o inglês aqui kkkkkk AKA ou Also Known As significa 'Também conhecido como'")
+
+console.log("\n");
+
+function nomeIdadeEstilo (nome, idade, estiloMusical){
+    console.log("Nome: "+nome+"\n"+"Idade: "+idade+"\n"+"Estilo Musical: "+estiloMusical);
+}
+
+nomeIdadeEstilo("Marcos Paulo", "17", "Funk Rock (especialmente Red Hot Chili Peppers)");
